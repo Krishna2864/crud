3557661129746e6f615f9aea869caa98ef5508a9
Krishna2864 <75547302+Krishna2864@users.noreply.github.com>
2021-12-13 10:05:02 +0530

    Update form.py

--- prof/form.py
+++ prof/form.py
@@ -4,7 +4,7 @@ from  django.contrib.auth.models import  User
 from  django.forms import ModelForm
 from  .models import Userpro
 
-
+#create form for POST DATA
 class Pro(ModelForm):
     class Meta:
         model = Userpro
